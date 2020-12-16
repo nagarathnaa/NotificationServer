359e79e6eb6fb28d94d826e10ad3d738440462a6
Smruthirekha Biswal <smrutirekha.biswal@edgenetworks.in>
2020-12-16 15:35:06 +0530

    functionality,subfunctionality views are added

---
+++ DOEAssessmentApp/DOEAssessmentApp/DOE_views/functionality_view.py
@@ -0,0 +1,93 @@
+from flask import *
+from DOEAssessmentApp import db
+from DOEAssessmentApp.DOE_models.functionality_model import Functionality
+from DOEAssessmentApp.DOE_models.area_model import Area
+from DOEAssessmentApp.DOE_models.sub_functionality_model import Subfunctionality
+
+functionality_view = Blueprint('functionality_view', __name__)
+
+
+def mergedict(*args):
+    output = {}
+    for arg in args:
+        output.update(arg)
+    return output
+
+
+@functionality_view.route('/api/functionality', methods=['GET', 'POST'])
+def getAndPost():
+    try:
+        if request.method == "GET":
+            results = []
+            data = Functionality.query.all()
+            for user in data:
+                json_data = mergedict({'id': user.id}, {'name': user.name}, {'description': user.description},
+                                      {'retake_assessment_days': user.retake_assessment_days},
+                                      {'area_id': user.area_id},
+                                      {'proj_id': user.proj_id},
+                                      {'creationdatetime': user.creationdatetime},
+                                      {'updationdatetime': user.updationdatetime})
+                results.append(json_data)
+
+            return make_response(jsonify(results)), 200
+        elif request.method == "POST":
+            res = request.get_json(force=True)
+            func_name = res['name']
+            func_desc = res['description']
+            func_retake_assess = res['retake_assessment_days']
+            func_area_id = res['area_id']
+            func_pro_id = res['proj_id']
+
+            existing_functionality = Functionality.query.filter(Functionality.name == func_name,
+                                                                Functionality.area_id == func_area_id).one_or_none()
+            if existing_functionality is None:
+                funcins = Functionality(func_name, func_desc, func_retake_assess, func_area_id, func_pro_id)
+                db.session.add(funcins)
+                db.session.commit()
+                return make_response(
+                    jsonify({"msg": "Functionality successfully inserted."})), 201
+            else:
+                data_area = Area.query.filter_by(id=func_area_id).first()
+                return jsonify({
+                    "message": f"functionality {func_name} already exists for area {data_area.name}."})
+    except Exception as e:
+        return make_response(jsonify({"msg": str(e)})), 401
+
+
+@functionality_view.route('/api/updelfunctionality/', methods=['PUT', 'DELETE'])
+def updateAndDelete():
+    try:
+        res = request.get_json(force=True)
+        functionalityid = res['functionalityid']
+        data = Functionality.query.filter_by(id=functionalityid).first()
+        if data is None:
+            return jsonify({"message": "Incorrect ID"})
+        else:
+            if request.method == 'PUT':
+                func_name = res['name']
+                func_area_id = res['area_id']
+                existing_functionality = Functionality.query.filter(Functionality.name == func_name,
+                                                                    Functionality.area_id == func_area_id).one_or_none()
+                if existing_functionality is None:
+                    data.name = func_name
+                    db.session.add(data)
+                    db.session.commit()
+                    return jsonify({"message": f"functionality {func_name} successfully updated."})
+                else:
+                    data_area = Area.query.filter_by(id=func_area_id).first()
+                    return jsonify({"message": f"functionality {func_name} already exists for area {data_area.name}."})
+
+            elif request.method == 'DELETE':
+                db.session.delete(data)
+                db.session.commit()
+                data_subfunc = Subfunctionality.query.filter_by(func_id=functionalityid)
+                if data_subfunc is not None:
+                    for s in data_subfunc:
+                        db.session.delete(s)
+                        db.session.commit()
+                return jsonify({"message": f"Functionality with ID {functionalityid} successfully deleted."})
+
+
+
+    except Exception as e:
+        return make_response(jsonify({"msg": str(e)})), 401
