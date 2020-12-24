c5fa84be96c8ae10730d057e93fd99aaae71461f
Smrutirekha Biswal <smrutirekha-biswal@devopsenabler.com>
2020-12-24 20:39:59 +0530

    token based authentication configured on assessment,functionality,usermanagement,project_assign_to_manager,sub_functionalty

--- DOEAssessmentApp/DOEAssessmentApp/DOE_views/project_assignment_to_manager_view.py
+++ DOEAssessmentApp/DOEAssessmentApp/DOE_views/project_assignment_to_manager_view.py
@@ -1,6 +1,8 @@
 from flask import *
 from DOEAssessmentApp import db
 from DOEAssessmentApp.DOE_models.project_assignment_to_manager_model import Projectassignmenttomanager
+from DOEAssessmentApp.DOE_models.company_user_details_model import Companyuserdetails
+
 
 assigningprojectmanager = Blueprint('assigningprojectmanager', __name__)
 
@@ -15,30 +17,42 @@ def mergedict(*args):
 @assigningprojectmanager.route('/api/assigningprojectmanager', methods=['GET', 'POST'])
 def getAndPost():
     try:
-        if request.method == "GET":
-            results = []
-            data = Projectassignmenttomanager.query.all()
-            for user in data:
-                json_data = mergedict({'id': user.id}, {'emp_id': user.emp_id}, {'project_id': user.project_id},
-                                      {'status': user.status}, {'creationdatetime': user.creationdatetime},
-                                      {'updationdatetime': user.updationdatetime})
-                results.append(json_data)
-            return make_response(jsonify(results)), 200
-        elif request.method == "POST":
-            res = request.get_json(force=True)
-            pm_id = res['emp_id']
-            pm_project_id = res['project_id']
-            existing_projectmanager = Projectassignmenttomanager.query.filter(Projectassignmenttomanager.emp_id ==
-                                                                              pm_id,
-                                                                              Projectassignmenttomanager.project_id ==
-                                                                              pm_project_id).one_or_none()
-            if existing_projectmanager is None:
-                project_managers_in = Projectassignmenttomanager(pm_id, pm_project_id)
-                db.session.add(project_managers_in)
-                db.session.commit()
-                return make_response(jsonify({"msg": "project manager successfully assigned."})), 201
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = Companyuserdetails.decode_auth_token(auth_token)
+            if isinstance(resp, str):
+                if request.method == "GET":
+                    results = []
+                    data = Projectassignmenttomanager.query.all()
+                    for user in data:
+                        json_data = mergedict({'id': user.id}, {'emp_id': user.emp_id}, {'project_id': user.project_id},
+                                              {'status': user.status}, {'creationdatetime': user.creationdatetime},
+                                              {'updationdatetime': user.updationdatetime})
+                        results.append(json_data)
+                    return make_response(jsonify(results)), 200
+                elif request.method == "POST":
+                    res = request.get_json(force=True)
+                    pm_id = res['emp_id']
+                    pm_project_id = res['project_id']
+                    existing_projectmanager = Projectassignmenttomanager.query.filter(Projectassignmenttomanager.emp_id ==
+                                                                                      pm_id,
+                                                                                      Projectassignmenttomanager.project_id ==
+                                                                                      pm_project_id).one_or_none()
+                    if existing_projectmanager is None:
+                        project_managers_in = Projectassignmenttomanager(pm_id, pm_project_id)
+                        db.session.add(project_managers_in)
+                        db.session.commit()
+                        return make_response(jsonify({"msg": "project manager successfully assigned."})), 201
+                    else:
+                        return jsonify({"msg": f"project manager was already assigned before."})
             else:
-                return jsonify({"msg": f"project manager was already assigned before."})
+                return jsonify({"msg": resp})
+        else:
+            return jsonify({"msg": "Provide a valid auth token."})
     except Exception as e:
         return make_response(jsonify({"msg": str(e)})), 401
 
@@ -46,23 +60,35 @@ def getAndPost():
 @assigningprojectmanager.route('/api/associateprojectmanager/', methods=['PUT'])
 def updateAndDelete():
     try:
-        res = request.get_json(force=True)
-        row_id = res['row_id']
-        data = Projectassignmenttomanager.query.filter_by(id=row_id).first()
-        if data is None:
-            return jsonify({"msg": "Incorrect ID"})
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
         else:
-            if request.method == 'PUT':
-                associate_status = res['associate_status']
-                if associate_status == 1:
-                    data.status = 1
-                    db.session.add(data)
-                    db.session.commit()
-                    return jsonify({"msg": "project manager associated successfully "})
+            auth_token = ''
+        if auth_token:
+            resp = Companyuserdetails.decode_auth_token(auth_token)
+            if isinstance(resp, str):
+                res = request.get_json(force=True)
+                row_id = res['row_id']
+                data = Projectassignmenttomanager.query.filter_by(id=row_id).first()
+                if data is None:
+                    return jsonify({"msg": "Incorrect ID"})
                 else:
-                    data.status = 0
-                    db.session.add(data)
-                    db.session.commit()
-                    return jsonify({"msg": "project manager disassociated successfully"})
+                    if request.method == 'PUT':
+                        associate_status = res['associate_status']
+                        if associate_status == 1:
+                            data.status = 1
+                            db.session.add(data)
+                            db.session.commit()
+                            return jsonify({"msg": "project manager associated successfully "})
+                        else:
+                            data.status = 0
+                            db.session.add(data)
+                            db.session.commit()
+                            return jsonify({"msg": "project manager disassociated successfully"})
+            else:
+                return jsonify({"msg": resp})
+        else:
+            return jsonify({"msg": "Provide a valid auth token."})
     except Exception as e:
         return e
--- DOEAssessmentApp/DOEAssessmentApp/DOE_views/user_management_view.py
+++ DOEAssessmentApp/DOEAssessmentApp/DOE_views/user_management_view.py
@@ -15,25 +15,38 @@ def mergedict(*args):
 @user_management_view.route('/api/usermanagement', methods=['GET', 'POST'])
 def getAndPost():
     try:
-        if request.method == "GET":
-            results = []
-            data = Companyuserdetails.query.all()
-            for user in data:
-                json_data = mergedict({'empid': user.empid}, {'empname': user.empname}, {'emprole': user.emprole},
-                                      {'empemail': user.empemail}, {'companyid': user.companyid},
-                                      {'emppasswordhash': user.emppasswordhash})
-                results.append(json_data)
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = Companyuserdetails.decode_auth_token(auth_token)
+            if isinstance(resp, str):
+                if request.method == "GET":
+                    results = []
+                    data = Companyuserdetails.query.all()
+                    for user in data:
+                        json_data = mergedict({'empid': user.empid}, {'empname': user.empname},
+                                              {'emprole': user.emprole},
+                                              {'empemail': user.empemail}, {'companyid': user.companyid},
+                                              {'emppasswordhash': user.emppasswordhash})
+                        results.append(json_data)
 
-            return make_response(jsonify(results)), 200
-        elif request.method == "POST":
-            res = request.get_json(force=True)
-            user_name = res['empname']
-            usermanagement = Companyuserdetails(res['empid'], res['empname'], res['emprole'], res['empemail'],
-                                                res['emppasswordhash'], res['companyid'])
-            db.session.add(usermanagement)
-            db.session.commit()
-            return make_response(
-                jsonify({"msg": f"UserManagement {user_name} successfully inserted."})), 201
+                    return make_response(jsonify(results)), 200
+                elif request.method == "POST":
+                    res = request.get_json(force=True)
+                    user_name = res['empname']
+                    usermanagement = Companyuserdetails(res['empid'], res['empname'], res['emprole'], res['empemail'],
+                                                        res['emppasswordhash'], res['companyid'])
+                    db.session.add(usermanagement)
+                    db.session.commit()
+                    return make_response(
+                        jsonify({"msg": f"UserManagement {user_name} successfully inserted."})), 201
+            else:
+                return jsonify({"msg": resp})
+        else:
+            return jsonify({"msg": "Provide a valid auth token."})
 
     except Exception as e:
         return make_response(jsonify({"msg": str(e)})), 401
@@ -42,27 +55,39 @@ def getAndPost():
 @user_management_view.route('/api/updelusermanagement/', methods=['PUT', 'DELETE'])
 def updateAndDelete():
     try:
-
-        res = request.get_json(force=True)
-        usermanagementid = res['usermanagementid']
-        data = Companyuserdetails.query.filter_by(id=usermanagementid).first()
-        if data is None:
-            return jsonify({"msg": "Incorrect ID"})
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
         else:
-            if request.method == 'PUT':
-                user_name = res['empname']
-                user_emppasswordhash = res['emppasswordhash']
-                data.emppasswordhash = user_emppasswordhash
-                data.empname = user_name
-                db.session.add(data)
-                db.session.commit()
-                return jsonify({"msg": f"UserManagement {user_name} successfully updated."})
+            auth_token = ''
+        if auth_token:
+            resp = Companyuserdetails.decode_auth_token(auth_token)
+            if isinstance(resp, str):
+
+                res = request.get_json(force=True)
+                usermanagementid = res['usermanagementid']
+                data = Companyuserdetails.query.filter_by(id=usermanagementid).first()
+                if data is None:
+                    return jsonify({"msg": "Incorrect ID"})
+                else:
+                    if request.method == 'PUT':
+                        user_name = res['empname']
+                        user_emppasswordhash = res['emppasswordhash']
+                        data.emppasswordhash = user_emppasswordhash
+                        data.empname = user_name
+                        db.session.add(data)
+                        db.session.commit()
+                        return jsonify({"msg": f"UserManagement {user_name} successfully updated."})
 
 
-            elif request.method == 'DELETE':
-                db.session.delete(data)
-                db.session.commit()
-                return jsonify({"msg": f"UserManagement with ID {usermanagementid} successfully deleted."})
+                    elif request.method == 'DELETE':
+                        db.session.delete(data)
+                        db.session.commit()
+                        return jsonify({"msg": f"UserManagement with ID {usermanagementid} successfully deleted."})
+            else:
+                return jsonify({"msg": resp})
+        else:
+            return jsonify({"msg": "Provide a valid auth token."})
 
     except Exception as e:
         return make_response(jsonify({"msg": str(e)})), 401
