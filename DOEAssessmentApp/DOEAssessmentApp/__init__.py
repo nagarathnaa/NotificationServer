88b9dc8acf95a2e615fae156213ea929499c6775
srijibbhattacharyya <srijib-bhattacharyya@devopsenabler.com>
2021-01-15 16:48:30 +0530

    code snippets for db migration segregated from wsgi.py and __init__.py to manage.py

--- DOEAssessmentApp/DOEAssessmentApp/__init__.py
+++ DOEAssessmentApp/DOEAssessmentApp/__init__.py
@@ -6,8 +6,6 @@ import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
 from DOEAssessmentApp import configs
 
@@ -37,11 +35,6 @@ db = SQLAlchemy(app)
 
 # Init ma
 ma = Marshmallow(app)
-
-migrate = Migrate(app, db)
-migrate.init_app(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 
 def create_app(blue_print_list=None):
--- DOEAssessmentApp/wsgi.py
+++ DOEAssessmentApp/wsgi.py
@@ -1,5 +1,3 @@
 from runserver import app
-from DOEAssessmentApp import manager
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
-    manager.run()
