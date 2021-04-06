53aaf8c28dfb279e1b87954641b19f193f71eb69
srijibbhattacharyya <srijib-bhattacharyya@devopsenabler.com>
2021-04-06 10:57:51 +0530

    API for fetch user profile data & change password implemented

--- DOEAssessmentApp/DOEAssessmentApp/DOE_views/user_management_view.py
+++ DOEAssessmentApp/DOEAssessmentApp/DOE_views/user_management_view.py
@@ -1,7 +1,7 @@
 from flask import *
 from DOEAssessmentApp import app, db
 from DOEAssessmentApp.DOE_models.company_user_details_model import Companyuserdetails
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from DOEAssessmentApp.DOE_models.audittrail_model import Audittrail
 
 user_management_view = Blueprint('user_management_view', __name__)
@@ -297,6 +297,93 @@ def fetchusersbyrole():
                         return make_response(jsonify({"data": result})), 200
                     else:
                         return make_response(jsonify({"msg": f"No users present with {user_role} role."})), 404
+            else:
+                return make_response(jsonify({"msg": resp})), 401
+        else:
+            return make_response(jsonify({"msg": "Provide a valid auth token."})), 401
+    except Exception as e:
+        return make_response(jsonify({"msg": str(e)})), 500
+
+
+def mergedict(*args):
+    output = {}
+    for arg in args:
+        output.update(arg)
+    return output
+
+
+@user_management_view.route('/api/fetchuserprofiledata', methods=['GET'])
+def fetchuserprofiledata():
+    try:
+        result = []
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = Companyuserdetails.decode_auth_token(auth_token)
+            if 'empid' in session and Companyuserdetails.query.filter_by(empemail=resp).first() is not None:
+                if request.method == "GET":
+                    data = Companyuserdetails.query.filter_by(empid=session['empid'])
+                    if data.first() is not None:
+                        for d in data:
+                            json_data = mergedict({'id': d.id},
+                                                  {'empid': d.empid},
+                                                  {'empname': d.empname},
+                                                  {'emprole': d.emprole},
+                                                  {'empemail': d.empemail})
+                            result.append(json_data)
+                        return make_response(jsonify({"data": result})), 200
+                    else:
+                        return make_response(jsonify({"message": "User does not exist !!"})), 404
+            else:
+                return make_response(jsonify({"msg": resp})), 401
+        else:
+            return make_response(jsonify({"msg": "Provide a valid auth token."})), 401
+    except Exception as e:
+        return make_response(jsonify({"msg": str(e)})), 500
+
+
+@user_management_view.route('/api/changepassword', methods=['PUT'])
+def changepassword():
+    try:
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = Companyuserdetails.decode_auth_token(auth_token)
+            if 'empid' in session and Companyuserdetails.query.filter_by(empemail=resp).first() is not None:
+                if request.method == "PUT":
+                    data = Companyuserdetails.query.filter_by(empid=session['empid'])
+                    result = [{col: getattr(d, col) for col in colsusermanagement} for d in data]
+                    userdatabefore = result[0]
+                    result.clear()
+                    if data.first() is not None:
+                        res = request.get_json(force=True)
+                        pwd = res['pwd']
+                        if check_password_hash(data.emppasswordhash, pwd):
+                            return make_response(jsonify({"message": "Please type a new password !!"})), 400
+                        else:
+                            data.first().emppasswordhash = generate_password_hash(pwd)
+                            data.first().modifiedby = session['empid']
+                            db.session.add(data.first())
+                            db.session.commit()
+                            data = Companyuserdetails.query.filter_by(empid=session['empid'])
+                            result = [{col: getattr(d, col) for col in colsusermanagement} for d in data]
+                            userdataafter = result[0]
+                            # region call audit trail method
+                            auditins = Audittrail("USER MANAGEMENT", "UPDATE", str(userdatabefore),
+                                                  str(userdataafter),
+                                                  session['empid'])
+                            db.session.add(auditins)
+                            db.session.commit()
+                            # end region
+                            return make_response(jsonify({"data": result})), 200
+                    else:
+                        return make_response(jsonify({"message": "User does not exist !!"})), 404
             else:
                 return make_response(jsonify({"msg": resp})), 401
         else:
