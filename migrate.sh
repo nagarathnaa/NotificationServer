1202c65b652faa65c9079a6c7ee0a750834fd2fc
srijibbhattacharyya <srijib-bhattacharyya@devopsenabler.com>
2021-04-14 11:20:18 +0530

    two user can have same username but not email and emp id check has been implemented

--- migrate.sh
+++ migrate.sh
@@ -2,8 +2,8 @@
 if [ -d "migrations" ]
 then
   echo "true"
-  python3 DOEAssessmentApp/manage.py db migrate; python3 DOEAssessmentApp/manage.py db upgrade
+  python3 manage.py db migrate; python3 manage.py db upgrade
 else
   echo "false"
-  python3 DOEAssessmentApp/manage.py db init; python3 DOEAssessmentApp/manage.py db migrate; python3 DOEAssessmentApp/manage.py db upgrade
+  python3 manage.py db init; python3 manage.py db migrate; python3 manage.py db upgrade
 fi
