09444440c48576c07b8540151e9cb1ec4a321c84
Smruthirekha Biswal <smrutirekha.biswal@edgenetworks.in>
2021-05-27 13:12:17 +0530

    modified the return statement format on trigger_notification.py

--- DOEAssessmentApp/trigger_notification.py
+++ DOEAssessmentApp/trigger_notification.py
@@ -1,5 +1,5 @@
 import requests
-from flask import jsonify, json
+from flask import json
 from DOEAssessmentApp import app
 from flask_socketio import SocketIO
 
@@ -17,17 +17,11 @@ def get_notification_data(notification):
     if notification['event_name'] in eventlist:
         API_ENDPOINT = 'http://127.0.0.1:5001/api/fetchnotificationdata/{}'.format(notification['event_name'])
         notification_data = requests.get(API_ENDPOINT)
-        # print("get data", type(notification_data))
-        print("get data", notification_data.text)
         json_data = json.loads(notification_data.text)
-        print("json_data", json_data)
-        print("role", json_data['data'][0]['role'])
-        print("app_notif_body", json_data['data'][0]['app_notif_body'])
-
-        return jsonify({"role": json_data['data'][0]['role'],
-                        "app_notif_body": json_data['data'][0]['app_notif_body']})
+        return {"role": json_data['data'][0]['role'],
+                "app_notif_body": json_data['data'][0]['app_notif_body']}
     else:
-        return jsonify({"message": "Notification event name not found"})
+        return {"message": "Notification event name not found"}
 
 
 @socketio.on('notification')
@@ -41,4 +35,3 @@ def handle_message(notification):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5002)
-    # app.run(debug=True, host='0.0.0.0', port=5001)
