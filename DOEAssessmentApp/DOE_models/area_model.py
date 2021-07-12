65a3dbd9c4e0b715946150cd6d011dd14db04b6d
srijibbhattacharyya <srijib-bhattacharyya@devopsenabler.com>
2021-07-12 19:29:03 +0530

    priority field added in area & functionality model and its corresponding changes in views

--- DOEAssessmentApp/DOE_models/area_model.py
+++ DOEAssessmentApp/DOE_models/area_model.py
@@ -6,6 +6,7 @@ class Area(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(180), nullable=False)
     description = db.Column(db.String(), nullable=False)
+    priority = db.Column(db.Integer)
     projectid = db.Column(db.Integer, nullable=False)
     assessmentcompletion = db.Column(db.Numeric())
     achievedpercentage = db.Column(db.Numeric())
@@ -17,11 +18,12 @@ class Area(db.Model):
     createdby = db.Column(db.String(20))
     modifiedby = db.Column(db.String(20))
 
-    def __init__(self, name, description, projectid, createdby):
+    def __init__(self, name, description, projectid, createdby, priority):
         self.name = name
         self.description = description
         self.projectid = projectid
         self.createdby = createdby
+        self.priority = priority
 
     def __repr__(self):
         return '<Area %r>' % self.name
