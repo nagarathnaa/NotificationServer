59ca39ec66cef126c0a3405f4e371d7f727c6490
Srijib Bhattacharyya <srijib-bhattacharyya@devopsenabler.com>
2021-07-14 12:47:28 +0000

    Deleted migrate.sh

--- migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -d "migrations" ]
-then
-  echo "true"
-  python3 manage.py db migrate; python3 manage.py db upgrade
-else
-  echo "false"
-  python3 manage.py db init; python3 manage.py db migrate; python3 manage.py db upgrade
-fi
